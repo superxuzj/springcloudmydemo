7d8c967a49e2b1c062fa6fc3703c086eef6e401f
superxuzj <1984708367@qq.com>
2021-06-01 21:39:48 +0800

    6.1

---
+++ test-unit/src/test/java/elk/controller/IndexControllerTest.java
@@ -0,0 +1,12 @@
+package elk.controller;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class IndexControllerTest {
+
+    @Test
+    public void testIndex() {
+    }
+}
