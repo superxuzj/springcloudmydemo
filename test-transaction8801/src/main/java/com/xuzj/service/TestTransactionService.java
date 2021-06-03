e2acb4ca3ccda34ed492dd41038e9577f7913223
xuzhijian <12123123@126.com>
2021-06-03 21:27:01 +0800

    6.3 add Transaction

--- test-transaction8801/src/main/java/com/xuzj/service/TestTransactionService.java
+++ test-transaction8801/src/main/java/com/xuzj/service/TestTransactionService.java
@@ -2,6 +2,8 @@ package com.xuzj.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 /*
 
 
@@ -33,7 +35,10 @@ public class TestTransactionService {
     @Autowired
     private SysroleService sysroleService;
 
+    @Transactional(rollbackFor = Exception.class)
     public void testTransactions(){
+        sysroleService.saveParent();
 
+        sysroleService.saveChild();
     }
 }
--- test-transaction8801/src/test/java/com/xuzj/test/TestMybatisPlusApplicationTests.java
+++ test-transaction8801/src/test/java/com/xuzj/test/TestMybatisPlusApplicationTests.java
@@ -1,7 +1,9 @@
 package com.xuzj.test;
 
+import com.xuzj.BaseTest;
 import com.xuzj.entity.Sysrole;
 import com.xuzj.mapper.SysroleMapper;
+import com.xuzj.service.TestTransactionService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,18 +14,14 @@ import java.util.List;
  * @author xuzhijian
  * @date 2021/6/2
  **/
-@SpringBootTest
-public class TestMybatisPlusApplicationTests {
+public class TestMybatisPlusApplicationTests extends BaseTest {
 
     @Autowired
-    private SysroleMapper sysroleMapper;
+    private TestTransactionService testTransactionService;
 
     @Test
     public void testSelect() {
         System.out.println(("----- selectAll method test ------"));
-        List<Sysrole> sysroleList = sysroleMapper.selectList(null);
-        for(Sysrole sysrole:sysroleList) {
-            System.out.println(sysrole);
-        }
+        testTransactionService.testTransactions();
     }
 }
